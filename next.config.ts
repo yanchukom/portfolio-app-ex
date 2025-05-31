dd56a13d32d05bab291158b5d53782156addacab
oyan4uk <oyanchuk.dev@gmail.com>
2025-05-31 19:33:55 +0300

    Test

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,6 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === "production";
-
 const nextConfig: NextConfig = {
-  basePath: isProd ? "/portfolio-app-ex" : "",
   output: "export",
   distDir: "dist",
   eslint: {
