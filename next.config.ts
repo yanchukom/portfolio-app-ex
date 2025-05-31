1c69d2b12b44785d144ab4e29b7b147ce3f6ba7b
oyan4uk <oyanchuk.dev@gmail.com>
2025-05-31 18:45:59 +0300

    Update next.config

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
+  basePath: isProd ? "/portfolio-app-ex" : "",
   output: "export",
   distDir: "dist",
   eslint: {
