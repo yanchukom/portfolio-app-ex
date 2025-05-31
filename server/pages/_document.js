22765a16436054e7903f9fec75261367f65cda5b
oyan4uk <oyanchuk.dev@gmail.com>
2025-05-31 17:30:10 +0300

    Updates

---
+++ server/pages/_document.js
@@ -0,0 +1,1 @@
+"use strict";(()=>{var e={};e.id=220,e.ids=[220],e.modules={361:e=>{e.exports=require("next/dist/compiled/next-server/pages.runtime.prod.js")},3873:e=>{e.exports=require("path")},4396:e=>{e.exports=require("react")},8732:e=>{e.exports=require("react/jsx-runtime")}};var r=require("../webpack-runtime.js");r.C(e);var s=e=>r(r.s=e),t=r.X(0,[520],()=>s(2520));module.exports=t})();
---
+++ static/chunks/app/resume/page-5317d10a26a50dd3.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[736,746,974],{813:()=>{}},_=>{var e=e=>_(_.s=e);_.O(0,[789,20,358],()=>e(813)),_N_E=_.O()}]);
