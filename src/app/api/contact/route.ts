067eb0649aa9c5820363f196ccc2c32254af88a0
oyan4uk <oyanchuk.dev@gmail.com>
2025-05-31 17:36:44 +0300

    Update packages

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -16,7 +16,7 @@ export async function POST(request: Request) {
     }
 
     // Save the message to the database
-    const newMessage = await prisma.message.create({
+    await prisma.message.create({
       data: {
         name,
         email,
